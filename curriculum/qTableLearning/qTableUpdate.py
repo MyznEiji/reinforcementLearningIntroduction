cee20830e22c6ca356d92a5e0a4aeb7dd2b26bd8
MyznEiji <em711th@icloud.com>
2018-02-06 17:22:43 +0900

    Fine DQN

--- curriculum/qTableLearning/qTableUpdate.py
+++ curriculum/qTableLearning/qTableUpdate.py
@@ -5,15 +5,22 @@ import numpy as np
 env = gym.make('FrozenLake-v0')
 Q = np.zeros([env.observation_space.n, env.action_space.n])
 
+# パラメータを更新する値
 lr = 0.8
+# ハイパーパラメータ
 y = 0.95
+# 思考する回数
 num_episodes = 2000
+# 報酬のリスト
 rList = []
 
 for i in range(num_episodes):
+    # ステートを初期化
     s = env.reset()
+    # 報酬の合計
     rAll = 0
     d = False
+    # エピソードあたりの移動数
     j = 0
     while j < 99:
         j += 1
@@ -26,3 +33,6 @@ for i in range(num_episodes):
         if d == True:
             break
     rList.append(rAll)
+
+print("回数ごとの結果:" + str(sum(rList)/num_episodes))
+print("最終的なQテーブルの値", "\n", Q)
